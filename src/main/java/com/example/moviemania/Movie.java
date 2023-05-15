6b909ee5171345d6de45a6656dc481cfeb4cb668
PrathikshaKini <kiniprathiksha18@gmail.com>
2023-05-15 23:55:01 +0530

    Entity Class + Simple controller implemented

---
+++ src/main/java/com/example/moviemania/Movie.java
@@ -0,0 +1,30 @@
+package com.example.moviemania;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+// @Data annotation takes care of all getters,setters and 2 string methods and comes from lombok dependency
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId id;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String>backdrops;
+    @DocumentReference
+    private List<Review>reviewIds;
+}
